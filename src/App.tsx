ec1e0e9d824dc9117c59696a391a83cd502092e4
Abdul Rehman <38237505+AbdulRehman197@users.noreply.github.com>
2021-05-08 23:23:33 +0500

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import "./styles.css";
+import * as PostalMime  from "postal-mime";
+console.log(new PostalMime.default())
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Hello CodeSandbox</h1>
+      <h2>Start editing to see some magic happen!</h2>
+    </div>
+  );
+}
